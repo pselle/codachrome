649c7bbf0bde6651680d2fae90f4edea27754436
pselle <pamela.selle@gmail.com>
2019-01-11 10:37:40 -0500

    Some refactoring on JS, moving init to user action

--- static/script.js
+++ static/script.js
@@ -1,37 +1,7 @@
-console.log("hey!");
-
-
-
-
-
-/* WebAudio API */
-
-// for legacy browsers
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-
-
-// create a new audio context
-const audioCtx = new AudioContext();
-
-// Try making an oscillator
-const osc = audioCtx.createOscillator();
-const gainNode = audioCtx.createGain();
-osc.type = 'sawtooth';
-osc.start();
-
-setInterval(function(){
-    document.getElementById("current-gain").innerText = Math.floor(gainNode.gain.value*100)/100;
-}, 10)
-
-// connect the oscillator to gain node, and the gain node to the audio context
-osc.connect(gainNode);
-gainNode.connect(audioCtx.destination);
-gainNode.gain.value = 1;
-
-
-
+let audioCtx, osc, gainNode;
 
 // midi stuff
+
 
 if (navigator.requestMIDIAccess) {
     console.log('This browser supports WebMIDI!');
@@ -43,68 +13,77 @@ if (navigator.requestMIDIAccess) {
 navigator.requestMIDIAccess()
     .then(onMIDISuccess, onMIDIFailure);
 
-
-
-
 octave = 4;
 
 var keysDown = {
-  "C": false,   
-  "D": false,   
-  "E": false,   
-  "F": false,   
-  "G": false,   
-  "A": false,   
-  "B": false,   
-  "C": false,   
+  "C": false,
+  "D": false,
+  "E": false,
+  "F": false,
+  "G": false,
+  "A": false,
+  "B": false,
+  "C": false,
 
-  "Db": false, 
-  "Eb": false, 
-  "Gb": false, 
-  "Ab": false, 
-  "Bb": false 
+  "Db": false,
+  "Eb": false,
+  "Gb": false,
+  "Ab": false,
+  "Bb": false
 }
 
 var userInputNotes = [];
 
-// initialize function that runs on page launch
+// initialize function that runs on user-init launch
 
-function init(){
+function initAudio(){
+  // for legacy browsers
+  console.log('init!')
+  const AudioContext = window.AudioContext || window.webkitAudioContext;
+
+
+  // create a new audio context
+  audioCtx = new AudioContext();
+
+  // Try making an oscillator
+  osc = audioCtx.createOscillator();
+  gainNode = audioCtx.createGain();
+  osc.type = 'sawtooth';
+  osc.start();
+
+
+  // connect the oscillator to gain node, and the gain node to the audio context
+  osc.connect(gainNode);
+  gainNode.connect(audioCtx.destination);
+  gainNode.gain.value = 0; // start silently
 
   // enable key listeners
-
+  console.log('a')
   enableKeyboardKeys();
-  enableSliders();
   enableRequestButton();
 
   var song = [63, 70, 78, 56, 34, 63];
-  //playArrayOfNotes(song);
+  playArrayOfNotes(song);
 
 }
-
-init();
-
-attackTime = 0;
-releaseTime = 0;
-
 
 function enableKeyboardKeys(){
 
 
     var keyboardPitchCodes = {
-      65 : "C",   
-      83 : "D",   
-      68 : "E",   
-      70 : "F",   
-      71 : "G",   
-      72 : "A",   
-      74 : "B",   
-      75 : "C",   
+      65 : "C",
+      83 : "D",
+      68 : "E",
+      70 : "F",
+      71 : "G",
+      72 : "A",
+      74 : "B",
+      75 : "C",
 
-      87 : "Db", 
-      69 : "Eb", 
-      84 : "Gb", 
-      89 : "Ab", 
+      87 : "Db",
+      69 : "Eb",
+      84 : "Gb",
+      89 : "Ab",
       85 : "Bb"
     }
 
@@ -115,7 +94,7 @@ function enableKeyboardKeys(){
 
         let noteInfo = codeInfo(code);
       // CALL FUNCTION TO PLAY SOUND ... or do whatever with it :)
-      
+
         keysDown[noteInfo.name] = (midiMessage.data[0] == 144) ? true : false;
 
 
@@ -123,7 +102,7 @@ function enableKeyboardKeys(){
       // ensure that our keydown doesn't keep triggering the same note again and again
       if(typeof(keyboardPitchCodes[e.keyCode]) != "undefined" && !keysDown[codeInfo(e.keyCode).name].playing){
           console.log("play " + codeInfo(e.keyCode).note);
-          
+
           gainNode.connect(audioCtx.destination);
           noteData[e.keyCode].playing = true;
 
@@ -140,25 +119,9 @@ function enableKeyboardKeys(){
             gainNode.gain.cancelScheduledValues(audioCtx.currentTime);
             gainNode.gain.linearRampToValueAtTime(0, audioCtx.currentTime + releaseTime);
           }
-          
       }
-      
     });
 }
-
-function enableSliders(){
-
-    document.querySelector("#attack").addEventListener("change", function(e){
-        attackTime = Number(e.target.value);
-        document.querySelector("#attack-value").innerText = e.target.value;
-    });
-
-    document.querySelector("#release").addEventListener("change", function(e){
-        releaseTime = Number(e.target.value);
-        document.querySelector("#release-value").innerText = e.target.value;
-    });
-}
-
 
 function enableRequestButton(){
     document.querySelector("#run").addEventListener("click", function(e){
@@ -194,9 +157,6 @@ function playFrequency(freq, velocity){
     gainNode.gain.value = 1;
     console.log(velocity);
 
-    // UNCOMMENT THIS TO USE ENVELOPES! 
-    // gainNode.gain.cancelScheduledValues(audioCtx.currentTime);
-    // gainNode.gain.linearRampToValueAtTime(velocity/127, audioCtx.currentTime + attackTime);
   }
 
 }
@@ -239,10 +199,10 @@ function getMIDIMessage(midiMessage) {
     let onOffCode = midiMessage.data[0];
     let pitchCode = midiMessage.data[1];
     let velocity = midiMessage.data[2]
-    
+
     let noteInfo = codeInfo(pitchCode);
   // CALL FUNCTION TO PLAY SOUND ... or do whatever with it :)
-      
+
     console.log(onOffCode + ", " + noteInfo.name);
     keysDown[noteInfo.name] = (onOffCode == 144) ? true : false;
 
@@ -250,7 +210,7 @@ function getMIDIMessage(midiMessage) {
     if(typeof(pitchCode) != "undefined" &&  onOffCode == 144){
       userInputNotes.push(pitchCode);
     }
-    
+
     playFrequency(noteInfo.frequency, velocity);
 }
 
@@ -262,13 +222,13 @@ function codeToFrequency(code) {
     let baselineFrequency = 27.5;
     let baselineCode = 21; // MIDI code for lowest note on keyboard
     let stepsAboveBaseline = code - baselineCode;
-    
+
     return baselineFrequency * (semitoneRatio ** stepsAboveBaseline);
 }
 
 
 
-// a function that takes a midi pitch code, and returns the frequency, octave info, and note name 
+// a function that takes a midi pitch code, and returns the frequency, octave info, and note name
 function codeInfo(code) {
 
     let notes = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"]
@@ -283,7 +243,7 @@ function codeInfo(code) {
     let thisFrequency = baselineFrequency * (semitoneRatio ** stepsAboveBaseline);
 
     let roundedFrequency = Math.floor(thisFrequency * 10000)/10000
-    
+
     return {
       name: thisNoteName,
       frequency: roundedFrequency,
@@ -308,8 +268,8 @@ function playNoteFromArray(arr){
 
   setTimeout(function(){
 
-      // simulate a MIDI keyboard 
-      
+      // simulate a MIDI keyboard
+
       var noteInfo = codeInfo(arr[0]);
 
       getMIDIMessage({data: [144, arr[0], 100]});
@@ -334,3 +294,7 @@ function playNoteFromArray(arr){
   }, 500)
 
 }
+
+document.getElementById('get-goin').addEventListener('click', function() {
+  initAudio();
+})
